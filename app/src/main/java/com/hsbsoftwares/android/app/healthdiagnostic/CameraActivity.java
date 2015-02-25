bc20aa608008f0788c005e33dc09efec87da8782
HSB <harpreetsingh.bola@gmail.com>
2015-02-25 17:18:23 +0100

    Initial commit.

---
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/CameraActivity.java
@@ -0,0 +1,125 @@
+package com.hsbsoftwares.android.app.healthdiagnostic;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Harpreet Singh Bola on 24/02/2015.
+ */
+public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
+    private static final String TAG = "CameraActivity";
+
+    private CameraView mOpenCvCameraView;
+    private List<Camera.Size> mResolutionList;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(CameraActivity.this);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public CameraActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_camera);
+
+        mOpenCvCameraView = (CameraView) findViewById(R.id.java_surface_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        ArrayList<View> views = new ArrayList<View>();
+        views.add(findViewById(R.id.processButton));
+        views.add(findViewById(R.id.settingsButton));
+        mOpenCvCameraView.addTouchables(views);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Log.i(TAG,"onTouch event");
+
+        mOpenCvCameraView.setResolution(mOpenCvCameraView.getResolutionList().get(3));//to be continued!!!!!!!!!!!!
+        return false;
+    }
+}
---
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/CameraView.java
@@ -0,0 +1,36 @@
+package com.hsbsoftwares.android.app.healthdiagnostic;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+
+import org.opencv.android.JavaCameraView;
+
+import java.util.List;
+
+/**
+ * Created by Harpreet Singh Bola on 24/02/2015.
+ */
+public class CameraView extends JavaCameraView {
+
+    private static final String TAG = "CameraView";
+
+    public CameraView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public List<Camera.Size> getResolutionList() {
+        return mCamera.getParameters().getSupportedPreviewSizes();
+    }
+
+    public void setResolution(Camera.Size resolution) {
+        disconnectCamera();
+        mMaxHeight = resolution.height;
+        mMaxWidth = resolution.width;
+        connectCamera(getWidth(), getHeight());
+    }
+
+    public Camera.Size getResolution() {
+        return mCamera.getParameters().getPreviewSize();
+    }
+}
