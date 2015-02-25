0a262f95574a2b7f2e0930422c7eb7a503a4cb66
HSB <harpreetsingh.bola@gmail.com>
2015-02-25 22:45:17 +0100

    Created detectMotion()2 method using Double Diff technique.

--- app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/MotionDetection.java
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/MotionDetection.java
@@ -44,4 +44,57 @@ public class MotionDetection {
 
         return resultFrame;
     }
+
+   /*
+    * Algorithm to detect motion using last three frames, this technique is called Double Diff.
+    * With this method the shadows created by the movement is deleted.
+    */
+    public Mat detectMotion2(final CameraBridgeViewBase.CvCameraViewFrame inputFrame, Mat previousFrame, Mat thirdLastFrame, Mat resultFrame, boolean firstTime, boolean secondTime ){
+        //Getting the current frame in gray.
+        final Mat currentGrayFrame = inputFrame.gray();
+        Mat differenceFrame = new Mat(currentGrayFrame.size(), currentGrayFrame.type());
+
+        if(firstTime){
+    		/*
+    		 * The first time i receive the frame it should also be a previous and as last third.
+    		 */
+            currentGrayFrame.copyTo(previousFrame);
+            currentGrayFrame.copyTo(thirdLastFrame);
+            firstTime = false;
+            Log.d(TAG, "First time processing: First frame set up as previous and third frame!");
+        }
+
+        if(secondTime){
+    		/*
+    		 * The second time the second frame should be set as third
+    		 */
+            previousFrame.copyTo(thirdLastFrame);
+            secondTime = false;
+        }
+
+        //Calculating the absolute difference between the current and previous frame.
+        Core.absdiff(thirdLastFrame, previousFrame, differenceFrame);//The difference between previousFrame and thirdLastFrame stored in differenceFrame.
+        //Image threshold: all the pixel values greater than 60 are converted to white (255) to better enhance the movement in the image.
+        Imgproc.threshold(differenceFrame, differenceFrame, 60, 255, Imgproc.THRESH_BINARY);
+
+        //Calculating the absolute difference between the current and previous frame.
+        Core.absdiff(previousFrame, currentGrayFrame, resultFrame);// Using resultFrame to store difference to avoid using unnecessary memory.
+        //Image threshold: all the pixel values greater than 60 are converted to white (255) to better enhance the movement in the image.
+        Imgproc.threshold(resultFrame, resultFrame, 60, 255, Imgproc.THRESH_BINARY);
+
+        //Doing bitwise and between two differences to optimize the motion detection algorithm, in this way we are avoiding
+        //the shadows/contrast created by movement
+        Core.bitwise_and(differenceFrame, resultFrame, resultFrame);
+        //Imgproc.threshold(mResultRgba, mResultRgba, 60, 255, Imgproc.THRESH_BINARY); // Doing something different??
+
+        ////saving the previous frame as last of three recent frames.
+        previousFrame.copyTo(thirdLastFrame);
+        //saving the current frame as previous.
+        currentGrayFrame.copyTo(previousFrame);
+
+        //Realising memory
+        currentGrayFrame.release();
+        differenceFrame.release();
+        return resultFrame;
+    }
 }
