18e6dba7f0cb284ab544ae280b35f8cd6e793f51
HSB <harpreetsingh.bola@gmail.com>
2015-02-26 15:01:03 +0100

    Created setBaseResolution.

--- app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/CameraView.java
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/CameraView.java
@@ -3,6 +3,7 @@ package com.hsbsoftwares.android.app.healthdiagnostic;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import org.opencv.android.JavaCameraView;
 
@@ -14,6 +15,9 @@ import java.util.List;
 public class CameraView extends JavaCameraView {
 
     private static final String TAG = "CameraView";
+
+    // The chosen resolution will be stored in mSize.
+    Camera.Size mSize;
 
     public CameraView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,5 +36,29 @@ public class CameraView extends JavaCameraView {
 
     public Camera.Size getResolution() {
         return mCamera.getParameters().getPreviewSize();
+    }
+
+    public void setBaseResolution() {
+        Camera.Parameters params = mCamera.getParameters();
+
+        // Check what resolutions are supported by your camera
+        List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+
+        // Iterate through all available resolutions and choose one.
+        for (Camera.Size size : sizes) {
+            Log.i(TAG, "Available resolution: " + size.width + " " + size.height);
+            if (size.width == 640 && size.height == 480) {
+                mSize = size;
+                break;
+            }
+        }
+
+        disconnectCamera();
+        mMaxHeight = mSize.height;
+        mMaxWidth = mSize.width;
+        connectCamera(getWidth(), getHeight());
+        //Log.i(TAG, "Chosen resolution: "+mSize.width+" "+mSize.height);
+        //params.setPictureSize(mSize.width, mSize.height);
+        //mCamera.setParameters(params);
     }
 }
