2de491b2eb23e948141a33b55c81f874897b2c02
HSB <harpreetsingh.bola@gmail.com>
2015-04-12 21:03:00 +0200

    Commented NativeMethods class.

--- app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/nativepack/NativeMethods.java
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/nativepack/NativeMethods.java
@@ -4,9 +4,10 @@ package com.hsbsoftwares.android.app.healthdiagnostic.nativepack;
  * Created by Harpreet Singh Bola on 20/03/2015.
  */
 public class NativeMethods {
-
+    //Constructor
     public NativeMethods(){
     }
 
+    //Native method for detecting Epilepsy and Clonic crisis
     public native int pathologyDetection(int[] lumVector);
 }
