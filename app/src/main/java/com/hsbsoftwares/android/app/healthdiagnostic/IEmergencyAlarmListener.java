6081c6c83d6b6b8cb3acdf8b23ff524f9a993861
HSB <harpreetsingh.bola@gmail.com>
2015-04-13 09:17:54 +0200

    Commented IEmergencyAlarmListener interface.

--- app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/IEmergencyAlarmListener.java
+++ app/src/main/java/com/hsbsoftwares/android/app/healthdiagnostic/IEmergencyAlarmListener.java
@@ -6,5 +6,6 @@ package com.hsbsoftwares.android.app.healthdiagnostic;
 //Interface: Observer pattern for creating custom listener for listening to crisis alarm
 public interface IEmergencyAlarmListener {
 
+    //Callback method that will be triggered on emergency occurred
     public void onEmergency();
 }
